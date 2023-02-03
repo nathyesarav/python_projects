88bc22981ad0ff778c7e35c76e626b7488eda3cd
Nathalie Saravia <nathye.sarav@gmail.com>
2023-02-03 20:34:07 +0100

    Actualizado

--- proyecto2/test.py
+++ proyecto2/test.py
@@ -9,6 +9,7 @@ for caracter in limpiar:
 texto = texto.replace("\n", " ")  # Reemplazo el salto de linea por " " para que no junte las palabras
 texto = texto.lower()  # Se pasa el texto a minúsculas, para no tener palabras repetidas en el diccionario
 palabras = texto.split(" ")  # Generamos una lista de palabras
+
 # Contamos las palabras y las guardamos en un listado diccionario de palabras únicas
 diccionario = {}
 for palabra in palabras:
@@ -28,7 +29,7 @@ def mayor(frecuencia):
 def main(frecuencia):
     print ("La palabra con mayor número es: ", mayor(frecuencia), " con ", frecuencia[mayor(frecuencia)], "apariciones")  
 
-main(diccionario);
+#main(diccionario);
 
 #Test2
 
